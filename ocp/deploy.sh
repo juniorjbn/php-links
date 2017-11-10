dd1ccab56c482db96ab9ed760d6bdae12b5e6c59
joao <juniorjbn@gmail.com>
2017-11-10 14:54:52 -0200

    still trying

--- ocp/deploy.sh
+++ ocp/deploy.sh
@@ -26,9 +26,9 @@ sed "
   s|__BRANCH_NAME__|$BRANCH_NAME|;
   " -i ocp/app.yml 
 
-oc new-app -f ocp/app.yml && echo $?
+oc new-app -f ocp/app.yml 
 
-oc start-build $ENV -F -w 
+oc start-build $ENV
 
 #oc rollout latest $ENV 
 
