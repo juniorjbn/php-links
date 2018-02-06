7879e9194abc4b1e466abfb33175f5d8915cd41e
joao <juniorjbn@gmail.com>
2018-02-06 09:49:21 -0200

    try new version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #8
+                    Clean Example #9
                     <br>
                 </div>
             </div>
