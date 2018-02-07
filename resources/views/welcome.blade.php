1470497cc6de1c7461a193bcb327eb5ec2778a2e
joao <juniorjbn@gmail.com>
2018-02-07 17:25:00 -0200

    indra

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #10
+                    Clean Example #11
                     <br>
                 </div>
             </div>
