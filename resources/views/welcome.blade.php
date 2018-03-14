2bc923b37ad20f28a04af22756b011c6559eee35
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-03-14 13:21:14 -0300

    Update for demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #16
+                    Clean Example #17
                     <br>
                 </div>
             </div>
