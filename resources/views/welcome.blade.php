f45c09fc6a89222f69c97b3af2457dfee7679e06
diogogoebel <diogo.goebel@gmail.com>
2018-05-22 18:13:47 -0300

    update 35 na vivo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #34                       <!-- Update here -->
+                    Clean Example #35                       <!-- Update here -->
                     <br>
                 </div>
             </div>
