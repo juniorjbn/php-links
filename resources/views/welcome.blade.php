d4e8fc81abc65ca81dd05378bea87e4d6d438a06
joao <juniorjbn@gmail.com>
2018-05-22 09:54:18 -0300

    update 33

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #28                       <!-- Update here -->
+                    Clean Example #33                       <!-- Update here -->
                     <br>
                 </div>
             </div>
