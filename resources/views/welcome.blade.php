8794da5d0d208f8c1df1180177c9bc2e04d78f90
diogogoebel <diogo.goebel@gmail.com>
2018-10-05 10:37:57 -0300

    45

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #44                  <!-- Update here -->
+                    Clean Example #45                  <!-- Update here -->
                     <br>
                 </div>
             </div>
