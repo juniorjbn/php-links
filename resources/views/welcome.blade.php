6d72ae6b57f8d8aab42e6ebdcba4679d21589acd
Joao Brito do nascimento junior <juniorjbn@Joaos-MacBook-Pro-2.local>
2019-04-16 15:33:11 -0300

    melhor numero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #25                  <!-- Update here -->
+                    Clean Example #12                  <!-- Update here -->
                     <br>
                 </div>
             </div>
