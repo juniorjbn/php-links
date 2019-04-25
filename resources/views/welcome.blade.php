525163c16940e61298065c5115eef1fb3653d2ef
Joao Brito do nascimento junior <juniorjbn@Joaos-MacBook-Pro-2.local>
2019-04-25 16:32:28 -0300

    99 com wasabi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #22                  <!-- Update here -->
+                    Clean Example #99                  <!-- Update here -->
                     <br>
                 </div>
             </div>
