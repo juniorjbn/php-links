213109e5a116f73d96a4835225d6ba4bc174cadf
Joao Brito do nascimento junior <juniorjbn@Joaos-MacBook-Pro-2.local>
2019-05-07 13:48:46 -0300

    finalmente 100

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #99                  <!-- Update here -->
+                    Clean Example #100                  <!-- Update here -->
                     <br>
                 </div>
             </div>
