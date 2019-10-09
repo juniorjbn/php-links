a7526f37a419562bb49175e65f8006f06014ed36
joao brito junior <juniorjbn@gmail.com>
2019-10-09 13:49:51 -0300

    update 102

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #101                  <!-- Update here -->
+                    Clean Example #102                  <!-- Update here -->
                     <br>
                 </div>
             </div>
