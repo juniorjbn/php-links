8b1197fd134dbb8298cc18760738514709ab8c20
joao brito <joao.brito@infra.elo>
2019-10-22 16:13:09 -0300

    update index para 104

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #103                  <!-- Update here -->
+                    Clean Example #104                  <!-- Update here -->
                     <br>
                 </div>
             </div>
